8b58eae18cc7da1606a27601b1b248d703061941
berlinfog <k1075060506@gmail.com>
2021-01-22 16:31:14 +0800

    20200122

---
+++ myblogvue/src/components/note/SideMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-menu
+    class="categories"
+    default-active="0"
+    @select="handleSelect"
+    active-text-color="red">
+    <el-menu-item index="0">
+      <i class="el-icon-menu"></i>
+      <span slot="title">全部</span>
+    </el-menu-item>
+    <el-menu-item index="1">
+      <i class="el-icon-menu"></i>
+      <span slot="title">影视</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <span slot="title">图书</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <i class="el-icon-menu"></i>
+      <span slot="title">音乐</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-menu"></i>
+      <span slot="title">文件</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+  export default {
+    name: 'SideMenu'
+  }
+</script>
+
+<style scoped>
+  .categories {
+    position: fixed;
+    margin-left: 50%;
+    left: -600px;
+    top: 100px;
+    width: 150px;
+  }
+</style>
+
